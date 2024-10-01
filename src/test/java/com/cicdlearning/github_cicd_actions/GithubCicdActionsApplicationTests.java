d21a2ed64131f3ea5f10995a9706b8307ab30846
kanyutu707 <kanyutualfred707@gmail.com>
2024-10-01 08:57:16 +0300

    code

---
+++ src/test/java/com/cicdlearning/github_cicd_actions/GithubCicdActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicdlearning.github_cicd_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
